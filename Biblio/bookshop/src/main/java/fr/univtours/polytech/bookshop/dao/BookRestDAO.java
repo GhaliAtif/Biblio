d07e81c1081251bbdd7dfd1130492881f06f23e4
ZakariaZiko <153945421+zakariaAtlas@users.noreply.github.com>
2024-06-05 14:35:41 +0200

    Add files via upload

---
+++ Biblio/bookshop/src/main/java/fr/univtours/polytech/bookshop/dao/BookRestDAO.java
@@ -0,0 +1,11 @@
+package fr.univtours.polytech.bookshop.dao;
+
+import java.util.List;
+
+import fr.univtours.polytech.bookshop.model.books.Doc;
+
+public interface BookRestDAO {
+
+   public List<Doc> getDocs(String search); 
+
+}
