6f75c408cd4ed3b174524b68cbb75af38c2c1974
ZakariaZiko <153945421+zakariaAtlas@users.noreply.github.com>
2024-06-05 14:41:18 +0200

    Add files via upload

---
+++ Biblio/bookshop/src/main/java/fr/univtours/polytech/bookshop/model/BookBean.java
@@ -0,0 +1,87 @@
+package fr.univtours.polytech.bookshop.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "BOOK")
+public class BookBean implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String author;
+    private Float price;
+    private String currency;
+    private int ratingsCount;
+    private Double ratingsAverage;
+    private String firstSentence;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public int getRatingsCount() {
+        return ratingsCount;
+    }
+
+    public void setRatingsCount(int ratingsCount) {
+        this.ratingsCount = ratingsCount;
+    }
+
+    public Double getRatingsAverage() {
+        return ratingsAverage;
+    }
+
+    public void setRatingsAverage(Double ratingsAverage) {
+        this.ratingsAverage = ratingsAverage;
+    }
+
+    public String getFirstSentence() {
+        return firstSentence;
+    }
+
+    public void setFirstSentence(String firstSentence) {
+        this.firstSentence = firstSentence;
+    }
+}
